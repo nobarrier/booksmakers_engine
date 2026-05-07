d19efcbf5fd70b73978518ce49fe6eea949137c1
Your Name <you@example.com>
2026-05-08 08:23:29 +0900

    Move Uncategorized category to end

--- apps/catalog/context_processors.py
+++ apps/catalog/context_processors.py
@@ -1,5 +1,22 @@
-from .models import Category
+from django.db.models import Case, IntegerField, Value, When
+
+from apps.catalog.models import Category
 
 
 def global_categories(request):
-    return {"categories": Category.objects.filter(parent__isnull=True)}
+    categories = (
+        Category.objects.filter(parent=None, is_active=True)
+        .annotate(
+            uncategorized_last=Case(
+                When(name="Uncategorized", then=Value(1)),
+                default=Value(0),
+                output_field=IntegerField(),
+            )
+        )
+        .prefetch_related("children")
+        .order_by("uncategorized_last", "name")
+    )
+
+    return {
+        "categories": categories,
+    }
